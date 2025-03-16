f67310346d84a32a3f8f6004ef16dc285f7f076b
namitha2526 <nnnamitha26@gmail.com>
2025-03-16 10:33:00 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css'
+import App from './components/App';
+import {AuthProvider} from "./components/contexts/authContext";
+import {BlockProvider} from "./components/contexts/blockContext";
+import {HashRouter} from "react-router-dom";
+
+ReactDOM.render( <HashRouter>
+<BlockProvider>
+<AuthProvider>
+<App />
+</AuthProvider>
+</BlockProvider>
+</HashRouter>,
+     document.getElementById('root'));
