7ae9598357fa4853e01e2ec23c73e1714f9175ab
NAMITHA R <nnnamitha26@gmail.com>
2025-08-26 22:49:09 +0530

    Update SupplyChain.sol

--- contracts/SupplyChain.sol
+++ contracts/SupplyChain.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 
 contract SupplyChain { 
     enum Role { None, Manufacturer, Retailer, Buyer }
-    enum State { Created, AtRetailer, Sold } 
+    enum State { Created, AtRetailer, Sold }  
   
     struct Product { 
         uint id; 
